0f0fb4d4ee446431458b7100f616243b2270692b
Kenzie Clarke <33361207+spinystarfish@users.noreply.github.com>
2020-09-23 10:26:45 -0500

    Update Aviation.java
    Added airportIATA method
    Changed airport method to airportICAO method

--- src/main/java/com/github/javafaker/Aviation.java
+++ src/main/java/com/github/javafaker/Aviation.java
@@ -22,10 +22,20 @@ public class Aviation {
      * Returns an airport ICAO code.
      * See also: https://en.wikipedia.org/wiki/List_of_airports_by_ICAO_code:_A
      */
-    public String airport() {
-        return faker.fakeValuesService().fetchString("aviation.airport");
+    public String airportICAO() {
+        return faker.fakeValuesService().fetchString("aviation.airportICAO");
     }
 
+    
+    /**
+     * Returns an airport IATA code.
+     * See also: https://en.wikipedia.org/wiki/List_of_airports_by_IATA_code:_A
+     */
+    public String airportIATA() {
+        return faker.fakeValuesService().fetchString("aviation.airportIATA");
+    }
+    
+    
     /**
      * Provides a METAR weather report.
      * Have a look at https://en.wikipedia.org/wiki/METAR
